11cd24df6e755733ab568a9a0c9bd73de93f8dbc
Jenny <205511984+jenny492@users.noreply.github.com>
2026-01-18 17:55:54 +0200

    Toimiva laskin

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
+// https://medium.com/@rl.myrie2/learn-react-native-with-me-building-a-simple-calculator-app-21b65fcf4d9b
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  
+
+  const [result, setResult] = useState('');
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text>Result: {result}</Text>
+        <TextInput style={styles.input} name="number1" inputMode='numeric' value={number1} onChangeText={(e) => setNumber1(e)} />
+        <TextInput style={styles.input} name="number2" inputMode='numeric' value={number2} onChangeText={(e) => setNumber2(e)} />
+
+        <View style={styles.operators}>
+          <Button
+            title="+"
+            onPress={() => {
+              setResult(Number(number1) + Number(number2));
+            }}
+          />
+          <Button
+            title=" -"
+            onPress={() => {
+              setResult(Number(number1) - Number(number2));
+            }}
+          />
+
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </View>
+
   );
 }
 
@@ -17,4 +49,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '50%',
+  },
+  operators: {
+    flexDirection: 'row',
+    width: '20%',
+    marginTop: 20,
+    justifyContent: 'space-between',
+  }
 });
