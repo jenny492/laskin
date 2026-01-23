cd6a60b95e7fcaab08d39182447edf5c5c2f8d42
Jenny <205511984+jenny492@users.noreply.github.com>
2026-01-23 18:21:25 +0200

    Tehty laskulle oma funktio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -13,68 +13,75 @@ export default function App() {
 
   const [result, setResult] = useState('');
 
-  return (
-
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Text>Result: {result}</Text>
-        <TextInput
-          style={styles.input}
-          name="number1"
-          inputMode='numeric'
-          value={number1}
-          onChangeText={(e) => setNumber1(e)}
-        />
-        <TextInput
-          style={styles.input}
-          name="number2"
-          inputMode='numeric'
-          value={number2}
-          onChangeText={(e) => setNumber2(e)}
-        />
-        <View style={styles.operators}>
-          <Button
-            title="+"
-            onPress={() => {
-              setResult(Number(number1) + Number(number2));
-            }}
-          />
-          <Button
-            title=" -"
-            onPress={() => {
-              setResult(Number(number1) - Number(number2));
-            }}
-          />
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    </View>
-
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  content: {
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: '50%',
-  },
-  operators: {
-    flexDirection: 'row',
-    width: '20%',
-    marginTop: 20,
-    justifyContent: 'space-between',
+  const calculate = (operation) => {
+    if (operation === '+') {
+      setResult(Number(number1) + Number(number2))
+    } else {
+      setResult(Number(number1) - Number(number2));
+    }
   }
-});
+
+    return (
+
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text>Result: {result}</Text>
+          <TextInput
+            style={styles.input}
+            name="number1"
+            inputMode='numeric'
+            value={number1}
+            onChangeText={(e) => setNumber1(e)}
+          />
+          <TextInput
+            style={styles.input}
+            name="number2"
+            inputMode='numeric'
+            value={number2}
+            onChangeText={(e) => setNumber2(e)}
+          />
+          <View style={styles.operators}>
+            <Button
+              title="+"
+              onPress={() => calculate('+')}
+            />
+            <Button
+              title=" -"
+              onPress={() => calculate('-')}
+            />
+          </View>
+          <View>
+            
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      </View>
+
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    content: {
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      width: '50%',
+    },
+    operators: {
+      flexDirection: 'row',
+      width: '20%',
+      marginTop: 20,
+      justifyContent: 'space-between',
+    }
+  });
